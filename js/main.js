2f7036048aa4658a25708def56dc2300e22ca4a4
taylous <taylous99@gmail.com>
2022-06-06 23:11:54 +0900

    implement scroll animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 (() => {
+  let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화된(눈 앞에 보고 있는) 씬(scroll-section)
+  let enterNewScene = false; // 새로운 scene이 시작되는 순간 true
+
   const sceneInfo = [
     {
       type: "sticky",
@@ -6,6 +11,13 @@
       scrollHeight: 0,
       objs: {
         container: document.querySelector("#scroll-section-0"),
+        messageA: document.querySelector("#scroll-section-0 .main-message.a"),
+        messageB: document.querySelector("#scroll-section-0 .main-message.b"),
+        messageC: document.querySelector("#scroll-section-0 .main-message.c"),
+        messageD: document.querySelector("#scroll-section-0 .main-message.d"),
+      },
+      values: {
+        messageA_opacity: [0, 1],
       },
     },
     {
@@ -40,9 +52,79 @@
       scene.scrollHeight = scene.heightNum * window.innerHeight;
       scene.objs.container.style.height = `${scene.scrollHeight}px`;
     });
-    console.log(sceneInfo);
+
+    yOffset = window.scrollY;
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i += 1) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+
+      if (totalScrollHeight < yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
   }
 
+  function calcValues(values, currentYOffset) {
+    let rv;
+    // 현재 scene에서 스크롤된 범위를 비율로 구하기
+    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+    rv = scrollRatio * (values[1] - values[0]) + values[0];
+    return rv;
+  }
+
+  function playAnimation() {
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    const currentYOffset = yOffset - prevScrollHeight;
+
+    switch (currentScene) {
+      case 0:
+        let messageA_opacity_in = calcValues(
+          values.messageA_opacity,
+          currentYOffset
+        );
+        objs.messageA.style.opacity = messageA_opacity_in;
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+        case3: break;
+    }
+  }
+
+  function scrollLoop() {
+    enterNewScene = false;
+
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i += 1) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      enterNewScene = true;
+      currentScene += 1;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
+    }
+
+    if (yOffset < prevScrollHeight) {
+      enterNewScene = true;
+      if (currentScene === 0) return;
+      currentScene -= 1;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
+    }
+
+    if (!enterNewScene) playAnimation();
+  }
+
+  window.addEventListener("scroll", () => {
+    yOffset = window.scrollY;
+    scrollLoop();
+  });
+  // DOM이 일단 load되면 실행
+  // window.addEventListener("DOMContentLoaded", setLayout);
+  window.addEventListener("load", setLayout);
   window.addEventListener("resize", setLayout);
-  setLayout();
 })();
